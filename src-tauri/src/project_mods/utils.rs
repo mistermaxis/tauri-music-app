6ec2e9f93aed9492e4b2af007f348f1f3da44036
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-06-11 01:26:45 -0300

    Add utils file to store useful functions

---
+++ src-tauri/src/project_mods/utils.rs
@@ -0,0 +1,12 @@
+pub fn capitalize_words(s: &str) -> String {
+    s.split_whitespace()
+        .map(|word| {
+            let mut chars = word.chars();
+            match chars.next() {
+                Some(first) => first.to_uppercase().collect::<String>() + chars.as_str(),
+                None => String::new(),
+            }
+        })
+        .collect::<Vec<String>>()
+        .join(" ")
+}
