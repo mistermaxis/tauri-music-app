8dcdff1b7d3e522c270537163ee06f18d6c080ab
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-06-11 01:29:52 -0300

    Implement audio file reading and info extracting using lofty crate

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,24 @@
+extern crate dirs;
+use crate::project_mods::{lofty::load_lofty_files, song::Song};
+
+mod project_mods;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_songs() -> Vec<project_mods::song::Song> {
+    let song_list = load_lofty_files();
+    match song_list {
+      Some(list) => list,
+      None => Vec::<Song>::new()
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_songs])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
